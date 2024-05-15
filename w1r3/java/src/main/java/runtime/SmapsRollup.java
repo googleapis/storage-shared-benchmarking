3a16030ea2b608bc022c71e0baa497d56bf65397
BenWhitehead <BenWhitehead@users.noreply.github.com>
2024-05-15 15:55:20 -0400

    chore(java): add java SmapsRollup utility (#114)
    To be wired into W1R3 in a later PR.

---
+++ w1r3/java/src/main/java/runtime/SmapsRollup.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package runtime;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Class that can be used to read the {@code /proc/PID/smaps_rollup} from <a
+ * href="https://man7.org/linux/man-pages/man5/proc.5.html">procfs</a> to determine the Resident Set
+ * Size (RSS) and Persistent Set Size (PSS) of the current process.
+ */
+public final class SmapsRollup {
+
+  private static final Path SMAPS_PATH = Paths.get("/proc/self/smaps_rollup");
+
+  private SmapsRollup() {}
+
+  public static SetSize get() throws IOException {
+    // read all lines completes in < 12us, if we were to try and do it ourselves with a channel
+    // it takes around 50us and comes with the disadvantage of having to do byte processing manually
+    List<String> strings = Files.readAllLines(SMAPS_PATH);
+    long rss = -1;
+    long pss = -1;
+    for (String s : strings) {
+      if (s.startsWith("Rss:")) {
+        String digits = s.substring(4, s.length() - 2).trim();
+        rss = Long.parseLong(digits);
+      } else if (s.startsWith("Pss:")) {
+        String digits = s.substring(4, s.length() - 2).trim();
+        pss = Long.parseLong(digits);
+      } else if (rss > -1 && pss > -1) {
+        break;
+      }
+    }
+    return new SetSize(rss, pss);
+  }
+}
