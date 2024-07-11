51c57af1e5fe27767d7eaba286a64ab92e32fac4
BenWhitehead <BenWhitehead@users.noreply.github.com>
2024-07-11 17:59:54 -0400

    chore(w1r3/java): refactor Instrumentation init to be encapsulated only depending on a provided Otel instance

--- w1r3/java/src/main/java/runtime/Instrumentation.java
+++ w1r3/java/src/main/java/runtime/Instrumentation.java
@@ -23,8 +23,10 @@ import io.opentelemetry.api.metrics.DoubleHistogram;
 import java.io.IOException;
 import java.lang.management.MemoryType;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import otel_support.Otel;
 import runtime.GcEvent.GcEventDiff;
 
 public final class Instrumentation {
@@ -35,7 +37,7 @@ public final class Instrumentation {
   private final DoubleHistogram cpuPerByteHistogram;
   private final DoubleHistogram allocatedBytesPerByteHistogram;
 
-  public Instrumentation(
+  private Instrumentation(
       DoubleHistogram latencyHistogram,
       DoubleHistogram cpuPerByteHistogram,
       DoubleHistogram allocatedBytesPerByteHistogram) {
@@ -47,6 +49,90 @@ public final class Instrumentation {
   public Measurement measure(long objectSize, Attributes attributes)
       throws IOException, InterruptedException {
     return new Measurement(objectSize, attributes);
+  }
+
+  public static Instrumentation create(Otel otel) {
+    var latencyHistogram =
+        otel.getBaseMeter()
+            .histogramBuilder("ssb/w1r3/latency")
+            .setExplicitBucketBoundariesAdvice(makeLatencyBoundaries())
+            .setUnit("s")
+            .build();
+
+    var cpuPerByteHistogram =
+        otel.getBaseMeter()
+            .histogramBuilder("ssb/w1r3/cpu")
+            .setExplicitBucketBoundariesAdvice(makeCpuBoundaries())
+            .setUnit("ns/By{CPU}")
+            .build();
+
+    var allocatedBytesPerByteHistogram =
+        otel.getBaseMeter()
+            .histogramBuilder("ssb/w1r3/memory")
+            .setExplicitBucketBoundariesAdvice(makeMemoryHistogramBoundaries())
+            .setUnit("1{memory}")
+            .build();
+
+    return new Instrumentation(
+        latencyHistogram, cpuPerByteHistogram, allocatedBytesPerByteHistogram);
+  }
+
+  /**
+   * We want millisecond-sized histogram buckets expressed in seconds. Floating point arithmetic is
+   * famously tricky, and time arithmetic is also error prone. Avoid most of these problems by using
+   * `Duration` to do all the arithmetic, and only convert to `double` (and seconds) when needed.
+   */
+  private static List<Double> makeLatencyBoundaries() {
+    var boundaries = new ArrayList<Double>();
+    var boundary = Duration.ofSeconds(0);
+    var increment = Duration.ofMillis(2);
+    for (int i = 0; i != 50; ++i) {
+      boundaries.add(boundary.toMillis() / 1000.0);
+      boundary = boundary.plus(increment);
+    }
+    boundary = Duration.ofMillis(100);
+    increment = Duration.ofMillis(10);
+    for (int i = 0; i != 150; ++i) {
+      if (boundary.compareTo(Duration.ofSeconds(300)) >= 0) break;
+      boundaries.add(boundary.toMillis() / 1000.0);
+      if (i != 0 && i % 10 == 0) increment = increment.multipliedBy(2);
+      boundary = boundary.plus(increment);
+    }
+    return boundaries;
+  }
+
+  private static List<Double> makeCpuBoundaries() {
+    int numBuckets = 200;
+    ArrayList<Double> boundaries = new ArrayList<>(numBuckets);
+    double boundary = 0.0;
+    double increment = 1.0 / 8.0;
+    for (int i = 0; i < numBuckets; i++) {
+      boundaries.add(boundary);
+      if (i != 0 && i % 32 == 0) {
+        increment *= 2;
+      }
+      boundary += increment;
+    }
+    return boundaries;
+  }
+
+  /**
+   * Cloud Monitoring only supports up to 200 buckets per histogram. Use exponentially growing
+   * bucket sizes
+   */
+  private static List<Double> makeMemoryHistogramBoundaries() {
+    int numBuckets = 200;
+    ArrayList<Double> boundaries = new ArrayList<>(numBuckets);
+    double boundary = 0.0;
+    double increment = 1.0 / 16.0;
+    for (int i = 0; i < numBuckets; i++) {
+      boundaries.add(boundary);
+      if (i != 0 && i % 16 == 0) {
+        increment *= 2;
+      }
+      boundary += increment;
+    }
+    return boundaries;
   }
 
   // when measuring we want to by symmetric in our evaluation of measurements.
